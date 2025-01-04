93e8e5502b55d94da11730518489c974a0832328
tracer-ics2019 <tracer@njuics.org>
2025-01-04 18:26:10 +0800

    >  compile U202111110 acer Linux newuserYXB 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:26:10 up  3:40,  1 user,  load average: 0.31, 0.18, 0.11 f2f1886d6651d553a1ed597f39f14853c2948c68

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -140,12 +140,8 @@ static int cmd_info(char *args) {
 }
 static int cmd_x(char *args) {
   int num,index;
-  printf("y");
+  printf("%d",sscanf(args,"%d 0x%-10x",&num,&index));
   return 0;
-  if(2!=sscanf(args,"%d 0x%-10x",&num,&index) ) {
-    printf("wrong arug");
-    return 0;
-  }
   if(num<=0) {
     printf("wrong\n");
     return 0;
