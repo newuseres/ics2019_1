8b3794f8962769217e86443c5c98792bff1a4f41
tracer-ics2019 <tracer@njuics.org>
2025-01-06 20:15:07 +0800

    >  run U202111110 acer Linux newuserYXB 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:15:07 up  6:22,  1 user,  load average: 0.16, 0.13, 0.10 dd7f32464c143c2c8431bf4274ea663211d48824

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -84,7 +84,11 @@ static bool make_token(char *e) {
   while (e[position] != '\0') {
     /* Try all rules one by one. */
     for (i = 0; i < NR_REGEX; i ++) {
+        Log("%d woc\n",i);
+
       if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
+              Log("%d woc\n",i);
+
         char *substr_start = e + position;
         int substr_len = pmatch.rm_eo;
 
