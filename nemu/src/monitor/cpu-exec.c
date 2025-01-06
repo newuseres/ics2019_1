b52fc96a0ae4cce61dc3a0392564b251247ed46f
tracer-ics2019 <tracer@njuics.org>
2025-01-06 21:20:17 +0800

    >  compile U202111110 acer Linux newuserYXB 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:20:17 up  7:33,  1 user,  load average: 0.24, 0.18, 0.11 5724683f9b0818734ff4fe6ef77b31e97ea28fcf

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -4,7 +4,6 @@
 #include "monitor/expr.h"
 
 
-extern WP *WP_head;
 /* The assembly code of instructions executed is only output to the screen
  * when the number of instructions executed is less than this value.
  * This is useful when you use the `si' command.
