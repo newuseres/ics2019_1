365fc71bcb3eaed38109a5641f36211cfdb9d4c4
tracer-ics2019 <tracer@njuics.org>
2025-01-06 21:00:47 +0800

    >  compile U202111110 acer Linux newuserYXB 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:00:47 up  7:12,  1 user,  load average: 0.11, 0.14, 0.10 88f54c72ed45c85d93d9472c190e963bc73837

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -181,7 +181,8 @@ static int cmd_w(char *args) {
   }
   WP *wp = new_wp();
   Log("why %s",args);
-//  assert(wp->value);
+  assert(wp);
+  assert(wp->value);
   Log("wyo");
   strcpy(wp->expr,args);
   
