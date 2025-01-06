643af93b7297cc975601417575c3e17907c7eea5
tracer-ics2019 <tracer@njuics.org>
2025-01-06 18:07:47 +0800

    >  compile U202111110 acer Linux newuserYXB 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:07:47 up  4:05,  1 user,  load average: 0.00, 0.02, 0.04 477388c4175c6169a161145cefa72fd2d064b45

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -5,7 +5,7 @@ int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
   int is_batch_mode = init_monitor(argc, argv);
 
-  TEST_expr();
+//  TEST_expr();
   /* Receive commands from user. */
   ui_mainloop(is_batch_mode);
 
