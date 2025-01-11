17a33b91462aea9a730e6c8a16688ef64cd8a43f
tracer-ics2019 <tracer@njuics.org>
2025-01-11 18:57:42 +0800

    >  compile U202111110 acer Linux newuserYXB 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:57:42 up  9:15,  1 user,  load average: 0.37, 0.35, 0.24 827b4d1a597e306e8ee91039fb6a709e5e09562

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -100,8 +100,7 @@ static int cmd_si(char *args){
 }
 
 void ui_mainloop(int is_batch_mode) {
-                    Log("MAP READ V FAIL");
-
+                    Log("MAP READ VV FAIL");
   if (is_batch_mode) {
     cmd_c(NULL);
     return;
