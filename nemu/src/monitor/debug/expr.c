facb5abb7d0fc60d19e7d87e619b35b7aaefd452
tracer-ics2019 <tracer@njuics.org>
2025-01-11 18:51:43 +0800

    >  compile U202111110 acer Linux newuserYXB 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:51:43 up  9:09,  1 user,  load average: 0.18, 0.17, 0.17 9b66da2faf43a8eb5a09e6e24022760e1d0d09c2

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -244,8 +244,6 @@ uint32_t calc(int i, int j, bool *success){
       if(! (*success) ) {
         return 0;
       }
-      Log("START CALC,0x%x",addr);
-      Log("WHY");
       return paddr_read(addr,4);
   } else if(5  == now_pri) {
       uint32_t num = calc(i+1,j,success);
