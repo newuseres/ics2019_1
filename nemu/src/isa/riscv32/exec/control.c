fa84a7945284fc5f5d5af47384a12b60fca3aec3
tracer-ics2019 <tracer@njuics.org>
2025-01-11 17:05:40 +0800

    >  compile U202111110 acer Linux newuserYXB 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:05:40 up  7:12,  1 user,  load average: 0.21, 0.16, 0.16 4bc0cc89b9735a821afb1732fb830783792eaca

--- nemu/src/isa/riscv32/exec/control.c
+++ nemu/src/isa/riscv32/exec/control.c
@@ -1,13 +1,13 @@
 #include "cpu/exec.h"
 
-make_EHelper(jal){
+static make_EHelper(jal){
     rtl_sr(id_dest->reg,&decinfo.seq_pc,4);
     rtl_j(id_src->val);
 
     print_asm_template2(jal);
 }
 
-make_EHelper(jalr){
+static make_EHelper(jalr){
     s0 = decinfo.seq_pc;
     rtl_sr(id_dest->reg,&s0,4);
     decinfo.jmp_pc = (id_src->val+id_src2->val)&(~1);
