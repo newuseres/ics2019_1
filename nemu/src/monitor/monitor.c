48878a3b2b450530f97f20b2727167d2dd7d24cd
tracer-ics2019 <tracer@njuics.org>
2025-01-11 11:28:08 +0800

    >  compile U202111110 acer Linux newuserYXB 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:28:08 up  2:25,  1 user,  load average: 0.16, 0.08, 0.05 64c47b1b79a8caf4340633c8f88d5cf5afa2e8f0

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -106,11 +106,10 @@ int init_monitor(int argc, char *argv[]) {
 
   /* Initialize differential testing. */
   init_difftest(diff_so_file, img_size);
-  printf("whe");
+  printf("wdasdasfasdadfshe");
 
   /* Display welcome message. */
   welcome();
-  printf("no play");
   return is_batch_mode;
 }
 
