4e8aad72092bcb6c62ee48e84b5cacd9aafc67fb
tracer-ics2019 <tracer@njuics.org>
2025-01-11 16:49:25 +0800

    >  compile U202111110 acer Linux newuserYXB 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:49:25 up  6:54,  1 user,  load average: 0.38, 0.25, 0.20 62a75450c7c4a79c439d7842511dcc3ac047a561

--- nemu/src/isa/riscv32/exec/exec.c
+++ nemu/src/isa/riscv32/exec/exec.c
@@ -9,6 +9,13 @@ static make_EHelper(load) {
   decinfo.width = load_table[decinfo.isa.instr.funct3].width;
   idex(pc, &load_table[decinfo.isa.instr.funct3]);
 }
+static OpcodeEntry calc_i_table [8] = {
+  EX(addi),EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY
+};
+
+static make_EHelper(calc_i) {
+  idex(pc, &calc_i_table[decinfo.isa.instr.funct3]);  
+}
 
 static OpcodeEntry store_table [8] = {
   EMPTY, EMPTY, EXW(st, 4), EMPTY, EMPTY, EMPTY, EMPTY, EMPTY
@@ -20,7 +27,7 @@ static make_EHelper(store) {
 }
 
 static OpcodeEntry opcode_table [32] = {
-  /* b00 */ IDEX(ld, load), EMPTY, EMPTY, EMPTY, IDEX(I,addi), IDEX(U,auipc), EMPTY, EMPTY,
+  /* b00 */ IDEX(ld, load), EMPTY, EMPTY, EMPTY, IDEX(I,calc_i), IDEX(U,auipc), EMPTY, EMPTY,
   /* b01 */ IDEX(st, store), EMPTY, EMPTY, EMPTY, EMPTY, IDEX(U, lui), EMPTY, EMPTY,
   /* b10 */ EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,
   /* b11 */ EMPTY, IDEX(I,jalr), EX(nemu_trap), IDEX(J,jal), EMPTY, EMPTY, EMPTY, EMPTY,
