f1ca28c00d2acbc8231feba55f042d3b07862ba9
tracer-ics2019 <tracer@njuics.org>
2025-01-11 18:59:29 +0800

    >  compile U202111110 acer Linux newuserYXB 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:59:29 up  9:17,  1 user,  load average: 0.17, 0.31, 0.24 198f561db36b4761533dccd823eb25775c4b1c2

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -64,22 +64,22 @@ void cpu_exec(uint64_t n) {
   log_clearbuf();
 
     /* TODO: check watchpoints here. */
-    // bool success;
-    // int res, flag = 0;
-    // for(WP *wp = WP_head; wp!=NULL; wp=wp->next) {
-    //   uint32_t new_value = expr(wp->expr, &success);
-    //   if(new_value != wp->value) {
-    //     flag = 1;
-    //     printf("Watchpoint %s change from %d to %d",wp->expr,wp->value,new_value);
-    //     wp->value = new_value;
-    //   }
-    // }
-    // if(flag == 1) {
-    //   printf("Watchpoint changed!\n");
-    //   if(nemu_state.state==NEMU_RUNNING) { //避免程序已经abort了还跳出来干别的
-    //     nemu_state.state = NEMU_STOP;
-    //   }
-    // }
+    bool success;
+    int res, flag = 0;
+    for(WP *wp = WP_head; wp!=NULL; wp=wp->next) {
+      uint32_t new_value = expr(wp->expr, &success);
+      if(new_value != wp->value) {
+        flag = 1;
+        printf("Watchpoint %s change from %d to %d",wp->expr,wp->value,new_value);
+        wp->value = new_value;
+      }
+    }
+    if(flag == 1) {
+      printf("Watchpoint changed!\n");
+      if(nemu_state.state==NEMU_RUNNING) { //避免程序已经abort了还跳出来干别的
+        nemu_state.state = NEMU_STOP;
+      }
+    }
 #endif
 
   g_nr_guest_instr ++;
@@ -91,6 +91,7 @@ void cpu_exec(uint64_t n) {
 
     if (nemu_state.state != NEMU_RUNNING) break;
   }
+                      Log("MAP READ VV FAIL");
 
   switch (nemu_state.state) {
     case NEMU_RUNNING: nemu_state.state = NEMU_STOP; break;
