0f1467daf40e5b9cdd3cc5bd275ef283384d685b
tracer-ics2019 <tracer@njuics.org>
2025-01-11 11:15:44 +0800

    >  compile U202111110 acer Linux newuserYXB 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:15:44 up  2:12,  1 user,  load average: 0.10, 0.05, 0.04 a0794f83854a9656631e5ff839553b035eb28aa6

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -97,7 +97,7 @@ void ui_mainloop(int is_batch_mode) {
     cmd_c(NULL);
     return;
   }
-  Log("Why");
+  printf("why");
   for (char *str; (str = rl_gets()) != NULL; ) {
     char *str_end = str + strlen(str);
 
