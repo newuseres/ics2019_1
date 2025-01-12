632f41874be8c1a013d7d463dfb8fc80b3fd76b1
tracer-ics2019 <tracer@njuics.org>
2025-01-12 19:56:48 +0800

    >  run U202111110 acer Linux newuserYXB 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:56:48 up 22:28,  1 user,  load average: 0.34, 0.24, 0.19 2b5f9e5f09e7589b73b0d8c4e20775f686a68cd

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -3,38 +3,83 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  return 0;
+  int i = 0;
+  while(s[i]){
+    i++;
+  }
+  return i;
 }
 
 char *strcpy(char* dst,const char* src) {
-  return NULL;
+  int i = 0;
+  for(;;i++) {
+    dst[i] = src[i];
+    if(!src[i]) break;
+  }
+  return dst;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
-  return NULL;
+  int i = 0;
+  for(;i<n;i++) {
+    dst[i] = src[i];
+    if(!src[i]) break;
+  }
+  return dst;
 }
 
 char* strcat(char* dst, const char* src) {
-  return NULL;
+  int len = strlen(dst);
+  int i=0;
+  for(;;i++){
+    dst[len+i] = src[i];
+    if(src[i]==0) break;  
+  }
+  return dst;
 }
 
 int strcmp(const char* s1, const char* s2) {
-  return 0;
+  int i=0;
+  for(;;i++) {
+    if(s1[i]==0&&s2[i]==0) return 0;
+    else if(s1[i]!=s2[i]) {
+      break;
+    }
+  }
+  return (s1[i] > s2[i] ? 1:-1 );
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
+  int i=0;
+  for(;i<n;i++) {
+    if(s1[i]==0&&s2[i]==0) return 0;
+    else if(s1[i]!=s2[i]) {
+      return (s1[i] > s2[i] ? 1:-1 );
+    }
+  }
   return 0;
 }
 
 void* memset(void* v,int c,size_t n) {
-  return NULL;
+  for(int i=0;i<n;i++) {
+      *((unsigned char*)v+i) = (unsigned char)c;
+  }
+  return v;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
+  for(int i=0;i<n;i++) {
+      *((unsigned char*)out+i) = *((unsigned char*)in+i);
+  }
   return NULL;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
+  for(int i=0;i<n;i++) {
+    if(*((unsigned char*)s1+i) != *((unsigned char*)s2+i)) {
+      *((unsigned char*)s1+i) > *((unsigned char*)s2+i);
+    }
+  }
   return 0;
 }
 
