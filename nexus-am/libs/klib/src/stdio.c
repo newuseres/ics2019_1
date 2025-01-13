72b169af0bc1f6e3745d8392be83ab58ce16c19f
tracer-ics2019 <tracer@njuics.org>
2025-01-13 11:30:01 +0800

    >  run U202111110 acer Linux newuserYXB 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:30:01 up 1 day, 14:10,  1 user,  load average: 0.31, 0.26, 0.26 f62c5fc055e2ef8015b736c3c3ec8269d61fa370

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -2,13 +2,15 @@
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
-
+//#define ZERO_SUPPLEMENT (1<<0)
+//#define DIGIT_GUARANTEED (1<<1)
 char num_set[] = "0123456789abcdef";
 
 int vsnprintf(char *out, size_t n,const char *fmt,va_list ap) {
   size_t putlen = 0;
   char stk[1<<10];
   int stk_top = 0;
+//   int flag = 0;
   while( *fmt != '\0' && putlen < n ) {
     if(*fmt == '%') {
       fmt++;
@@ -33,6 +35,8 @@ int vsnprintf(char *out, size_t n,const char *fmt,va_list ap) {
           break;
         }
         case 'x':{
+          out[putlen++] = '0';
+          out[putlen++] = 'x';
          uint32_t num = va_arg(ap,uint32_t);
           if(num == 0) out[putlen++] = '0';
           stk_top = 0;
@@ -73,6 +77,24 @@ int vsnprintf(char *out, size_t n,const char *fmt,va_list ap) {
         case 'c':{
           char c = va_arg(ap,int);
           out[putlen++] = c;
+          break;
+        }
+        case '0':{
+          fmt++;
+          out_put_numer_for_align(out,fmt,&putlen,ap,'0');
+          fmt++;
+          break;
+        }
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9': {
+          out_put_numer_for_align(out,fmt,putlen,ap,' ');
           break;
         }
       default:
@@ -126,6 +148,77 @@ int snprintf(char *out, size_t n, const char *fmt, ...) {
   int len = (int)vsnprintf(out,n,fmt,args);
   va_end(args);
   return len;
+}
+
+void out_put_numer_for_align(char *out, const char *fmt,int *putlen, va_list ap,char c) {
+  int align_digit = 0;
+  char stk[1024];
+  int stk_top = 0;
+  while(*fmt!='d'&&*fmt!='x'&&*fmt!='u') {
+    align_digit = align_digit * 10 + *fmt - '0';
+    fmt++;
+  }
+  if(*fmt == 'd' ) {
+    int32_t num = va_arg(ap,int32_t);
+    stk_top = 0;
+    if(num == 0) {
+      stk_top = 1;
+      stk[0] = 0;
+    }
+    if(num<0) {
+      out[*putlen++] = '-';
+      num = -num;
+    }
+    while(num) {
+      stk[stk_top++] = num%10 + '0';
+      num = num / 10;
+    }
+    for(int i=stk_top;i<align_digit;i++) {
+      out[*putlen++] = c;
+    }
+    while(stk_top) {
+      out[*putlen++] = stk[stk_top-1];
+      stk_top -= 1;
+    }
+  } else if(*fmt == 'u') {
+    uint32_t num = va_arg(ap,uint32_t);
+    stk_top = 0;
+    if(num == 0) {
+      stk_top = 1;
+      stk[0] = '0';
+    }
+    while(num) {
+      stk[stk_top++] = num%10 + '0';
+      num = num / 10;
+    }
+    for(int i=stk_top;i<align_digit;i++) {
+      out[*putlen++] = c;
+    }
+    while(stk_top) {
+      out[*putlen++] = stk[stk_top-1];
+      stk_top -= 1;
+    }    
+  } else if(*fmt=='x') {
+      out[*putlen++] = '0';
+      out[*putlen++] = 'x';
+      uint32_t num = va_arg(ap,uint32_t);
+      stk_top = 0;
+    if(num == 0) {
+      stk_top = 1;
+      stk[0] = '0';
+    }
+    while(num) {
+      stk[stk_top++] = num_set[num%16];
+      num = num / 16;
+    }
+    for(int i=stk_top;i<align_digit;i++) {
+      out[*putlen++] = c;
+    }
+    while(stk_top) {
+      out[*putlen++] = stk[stk_top-1];
+      stk_top -= 1;
+    }        
+  }
 }
 
 #endif
