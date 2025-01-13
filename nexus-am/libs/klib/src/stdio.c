f95e2036dd3c15d38bd12c02fa3d853beaf4aac5
tracer-ics2019 <tracer@njuics.org>
2025-01-13 10:32:35 +0800

    >  run U202111110 acer Linux newuserYXB 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:32:35 up 1 day, 13:08,  1 user,  load average: 0.40, 0.32, 0.27 af916f7a7994dba4d560eb40725feea0878ecd10

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -76,6 +76,7 @@ int vsnprintf(char *out, size_t n,const char *fmt,va_list ap) {
           break;
         }
       default:
+        printf("WTF %c",*fmt);
         assert(0&&"TO DO IN PRINTF");
         break;
       }
