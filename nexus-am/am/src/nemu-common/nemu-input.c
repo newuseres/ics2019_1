ad2780adc068d15b8c7b0de1e3013a654fee7af6
tracer-ics2019 <tracer@njuics.org>
2025-01-13 12:10:30 +0800

    >  run U202111110 acer Linux newuserYXB 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:10:30 up 1 day, 14:54,  1 user,  load average: 0.36, 0.49, 0.37 fbf6bfa3a4aa594ccc78ca70a89785708e7917de

--- nexus-am/am/src/nemu-common/nemu-input.c
+++ nexus-am/am/src/nemu-common/nemu-input.c
@@ -8,8 +8,8 @@ size_t __am_input_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_INPUT_KBD: {
       _DEV_INPUT_KBD_t *kbd = (_DEV_INPUT_KBD_t *)buf;
-      kbd->keycode = inl(KBD_ADDR);
-      kbd->keydown = kbd->keycode!=_KEY_NONE;
+      kbd->keycode = inl(KBD_ADDR) & (~KEYDOWN_MASK);
+      kbd->keydown = (kbd->keycode & KEYDOWN_MASK) ? 1 : 0;
       return sizeof(_DEV_INPUT_KBD_t);
     }
   }
