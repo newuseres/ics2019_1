c2358c6124b7ac44e6712a3d38084184f1c2d5ee
tracer-ics2019 <tracer@njuics.org>
2025-01-13 11:30:55 +0800

    >  run U202111110 acer Linux newuserYXB 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:30:55 up 1 day, 14:11,  1 user,  load average: 0.38, 0.28, 0.27 a59902f211a72226c31cc0377d2ac5ec868bd835

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -95,6 +95,7 @@ int vsnprintf(char *out, size_t n,const char *fmt,va_list ap) {
         case '8':
         case '9': {
           out_put_numer_for_align(out,fmt,putlen,ap,' ');
+          fmt++;
           break;
         }
       default:
